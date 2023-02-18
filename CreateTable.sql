7b0a5ce97fbdab2fcce682133607c1f4ce8c084b
momu54 <96234201+momu54@users.noreply.github.com>
2023-02-18 21:38:09 +0800

    (add) [CreateTable.sql] Store "Create table" code

---
+++ CreateTable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE config (
+    SaveAllImage_convert BOOLEAN DEFAULT 1,
+    ScreenShot_format TEXT DEFAULT "webp",
+    global_colot TEXT DEFAULT "#ffffff"
+);
+
+CREATE TABLE AnimeTodo (
+    user TEXT,
+    name TEXT,
+    an TEXT,
+    episode TEXT
+);
