3e07ce30ae96d8803f4ec8f9d2b7920877b75fc5
momu54 <96234201+momu54@users.noreply.github.com>
2023-02-26 10:59:27 +0800

    (fix) [CreateTable.sql] Fix typo

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE config (
     SaveAllImage_convert BOOLEAN DEFAULT 1,
-    ScreenShot_format TEXT DEFAULT "webp",
+    Screenhot_format TEXT DEFAULT "webp",
     global_color TEXT DEFAULT "#ffffff",
     user TEXT
 );
